2cfc6afa91e0e346ea2309e6b204e8a32fadc585
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:31:30 +0100

    feat: define textarea props

--- frontend/src/components/ui/Textarea.tsx
+++ frontend/src/components/ui/Textarea.tsx
@@ -2,3 +2,8 @@
 
 import { TextareaHTMLAttributes, forwardRef } from 'react';
 
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label?: string;
+  error?: string;
+}
+
