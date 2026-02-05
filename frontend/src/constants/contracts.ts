1e3cce79b8b3f5b8c631160ea6b8347609c57c6f
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:28:27 +0100

    feat: add contract names

--- frontend/src/constants/contracts.ts
+++ frontend/src/constants/contracts.ts
@@ -1,2 +1,9 @@
 // Contract Constants
 
+export const CONTRACT_NAMES = {
+  NFT_MARKETPLACE: "serenehub-nft-marketplace",
+  STAKING_VAULT: "serenehub-staking-vault",
+  TOKEN_LAUNCHPAD: "serenehub-token-launchpad",
+  SERVICE_REGISTRY: "serenehub-service-registry",
+} as const;
+
