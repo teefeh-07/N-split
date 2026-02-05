7699702619fc08c9a131dd2cfe59f9569f163fbb
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:31:43 +0100

    feat: add debounce function

--- frontend/src/lib/utils/debounce.ts
+++ frontend/src/lib/utils/debounce.ts
@@ -1,2 +1,14 @@
 // Debounce Utilities
 
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  return (...args: Parameters<T>) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), wait);
+  };
+}
+
