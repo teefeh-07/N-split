eec1a3692edb9037498739c7e76c64012d6dbee6
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:46 +0100

    feat: add stacks imports

--- frontend/src/hooks/useStaking.ts
+++ frontend/src/hooks/useStaking.ts
@@ -2,3 +2,7 @@
 
 import { useState, useCallback } from 'react';
 
+import { openContractCall } from '@stacks/connect';
+import { StacksTestnet } from '@stacks/network';
+import { uintCV, PostConditionMode } from '@stacks/transactions';
+
