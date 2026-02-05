5dd5cfc0879be16adf6d9452e8b233f1bad7f4a4
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:28:44 +0100

    feat: define hook interface

--- frontend/src/hooks/useService.ts
+++ frontend/src/hooks/useService.ts
@@ -6,3 +6,9 @@ import { openContractCall } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 import { stringAsciiCV, uintCV, PostConditionMode } from '@stacks/transactions';
 
+interface UseServiceResult {
+  registerService: (name: string, description: string, price: number) => Promise<void>;
+  payService: (serviceId: number) => Promise<void>;
+  isLoading: boolean;
+}
+
