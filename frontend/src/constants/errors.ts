abdaaaccf67c260c792e9757693fc11c5a1a5b86
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:28:24 +0100

    feat: add contract error codes

--- frontend/src/constants/errors.ts
+++ frontend/src/constants/errors.ts
@@ -1,2 +1,10 @@
 // Error Constants
 
+export const CONTRACT_ERRORS = {
+  UNAUTHORIZED: "u100",
+  INSUFFICIENT_FUNDS: "u101",
+  INVALID_PARAMS: "u102",
+  NOT_FOUND: "u103",
+  ALREADY_EXISTS: "u104",
+} as const;
+
