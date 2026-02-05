2c3e94292fc1bc4165876dcd8babc8a671f4879f
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:31:44 +0100

    feat: add throttle function

--- frontend/src/lib/utils/debounce.ts
+++ frontend/src/lib/utils/debounce.ts
@@ -12,3 +12,17 @@ export function debounce<T extends (...args: any[]) => any>(
   };
 }
 
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle = false;
+
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
