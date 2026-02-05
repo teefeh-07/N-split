0d2d26c823c811770850fc78c04b58a3c5c855d6
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:28:25 +0100

    feat: create network constants file

--- frontend/src/constants/networks.ts
+++ frontend/src/constants/networks.ts
@@ -1,22 +1,2 @@
 // Network Constants
 
-export enum Network {
-  MAINNET = 'mainnet',
-  TESTNET = 'testnet',
-  DEVNET = 'devnet',
-}
-
-export const NETWORK_CONFIG = {
-  [Network.MAINNET]: {
-    apiUrl: "https://stacks-node-api.mainnet.stacks.co",
-    explorerUrl: "https://explorer.stacks.co",
-  },
-  [Network.TESTNET]: {
-    apiUrl: "https://stacks-node-api.testnet.stacks.co",
-    explorerUrl: "https://explorer.stacks.co/?chain=testnet",
-  },
-  [Network.DEVNET]: {
-    apiUrl: "http://localhost:3999",
-    explorerUrl: "http://localhost:8000",
-  },
-} as const;
