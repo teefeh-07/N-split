5879a1241e628a946d2be954eb7f47768180df0e
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:31:54 +0100

    feat: add status type

--- frontend/src/types/ui.ts
+++ frontend/src/types/ui.ts
@@ -4,3 +4,5 @@ export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
 export type Variant = 'primary' | 'secondary' | 'outline' | 'ghost' | 'danger';
 
+export type Status = 'idle' | 'loading' | 'success' | 'error';
+
