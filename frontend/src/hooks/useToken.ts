ecc5ad08e053f853e880b2587961b874cd0530e4
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:55 +0100

    feat: define hook interface

--- frontend/src/hooks/useToken.ts
+++ frontend/src/hooks/useToken.ts
@@ -6,3 +6,8 @@ import { openContractCall } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 import { stringAsciiCV, uintCV, PostConditionMode } from '@stacks/transactions';
 
+interface UseTokenResult {
+  createToken: (name: string, symbol: string, supply: number) => Promise<void>;
+  isLoading: boolean;
+}
+
