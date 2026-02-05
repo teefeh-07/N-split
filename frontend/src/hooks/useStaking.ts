9d114514e15fe437cda0a536b113395cfdc90cd9
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:47 +0100

    feat: define hook interface

--- frontend/src/hooks/useStaking.ts
+++ frontend/src/hooks/useStaking.ts
@@ -6,3 +6,10 @@ import { openContractCall } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 import { uintCV, PostConditionMode } from '@stacks/transactions';
 
+interface UseStakingResult {
+  stake: (amount: number) => Promise<void>;
+  unstake: () => Promise<void>;
+  isLoading: boolean;
+  error: string | null;
+}
+
