bc45356f4c8c178963ee98963e044dedafb0f8d6
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:44 +0100

    feat: add microSTX converter

--- frontend/src/lib/utils/stacks.ts
+++ frontend/src/lib/utils/stacks.ts
@@ -6,3 +6,7 @@ export function getNetwork(isMainnet: boolean = false) {
   return isMainnet ? new StacksMainnet() : new StacksTestnet();
 }
 
+export function stxToMicro(stx: number): number {
+  return Math.floor(stx * 1_000_000);
+}
+
