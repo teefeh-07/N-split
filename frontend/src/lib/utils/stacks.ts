83c2ea51f8b0096e5245489a236ddc1fd498e7dc
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:43 +0100

    feat: create stacks utils file

--- frontend/src/lib/utils/stacks.ts
+++ frontend/src/lib/utils/stacks.ts
@@ -1,16 +1,2 @@
 // Stacks Utilities
 
-import { StacksMainnet, StacksTestnet } from '@stacks/network';
-
-export function getNetwork(isMainnet: boolean = false) {
-  return isMainnet ? new StacksMainnet() : new StacksTestnet();
-}
-
-export function stxToMicro(stx: number): number {
-  return Math.floor(stx * 1_000_000);
-}
-
-export function microToStx(micro: number): number {
-  return micro / 1_000_000;
-}
-
