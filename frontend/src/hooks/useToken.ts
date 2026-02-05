02eb161b9f35e74f7e39a2dc8052ab3d7b916866
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:54 +0100

    feat: add stacks imports

--- frontend/src/hooks/useToken.ts
+++ frontend/src/hooks/useToken.ts
@@ -2,3 +2,7 @@
 
 import { useState, useCallback } from 'react';
 
+import { openContractCall } from '@stacks/connect';
+import { StacksTestnet } from '@stacks/network';
+import { stringAsciiCV, uintCV, PostConditionMode } from '@stacks/transactions';
+
