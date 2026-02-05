a42a2766dd5f7fd9c403a503bc0c18d940d592c8
teefeh_07 <boluwatifeoniyilo@gmail.com>
2026-02-05 13:27:50 +0100

    feat: define hook interface

--- frontend/src/hooks/useNFT.ts
+++ frontend/src/hooks/useNFT.ts
@@ -6,3 +6,10 @@ import { openContractCall } from '@stacks/connect';
 import { StacksTestnet } from '@stacks/network';
 import { uintCV, stringAsciiCV, PostConditionMode } from '@stacks/transactions';
 
+interface UseNFTResult {
+  mintNFT: (uri: string) => Promise<void>;
+  listNFT: (tokenId: number, price: number) => Promise<void>;
+  buyNFT: (tokenId: number) => Promise<void>;
+  isLoading: boolean;
+}
+
